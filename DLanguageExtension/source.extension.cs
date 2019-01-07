6eac56a3468e4b92ab29f6e80c6aca3f01297e1e
Laurent Tréguier <laurent@treguier.org>
2019-01-07 13:47:28 +0100

    1.0.1

--- DLanguageExtension/source.extension.cs
+++ DLanguageExtension/source.extension.cs
@@ -11,7 +11,7 @@ namespace DLanguageExtension
         public const string Name = "Dlang";
         public const string Description = @"D language support for Visual Studio";
         public const string Language = "en-US";
-        public const string Version = "1.0.0";
+        public const string Version = "1.0.1";
         public const string Author = "Laurent Tréguier";
         public const string Tags = "d, dlang, lsp, language-server";
     }
